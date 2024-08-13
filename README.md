9f9777ac4bcd4897010c1f07c896fb388c7f6fe9
srikanthreddymallepally <42182325+SrikanthReddyMallepally@users.noreply.github.com>
2024-08-13 03:56:01 -0400

    Completed Version 1.0
    This is the finished version 1.0 commit to git repo

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# CodePathOrg_Prework
-this repo is the prework for WEB102
+# WEB102 Prework - _Sea Monster Crowdfunding_
+
+Submitted by: **Srikanth Reddy Mallepally**
+
+**Sea Monster Crowdfunding** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+
+Time spent: **20** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] The introduction section explains the background of the company and how many games remain unfunded.
+- [x] The Stats section includes information about the total contributions and dollars raised as well as the top two most funded games.
+- [x] Our Games section initially displays all games funded by Sea Monster Crowdfunding
+- [x] Our Games section has three buttons that allow the user to display only unfunded games, only funded games, or all games.
+
+The following **optional** features are implemented:
+
+- [x] search functionality is added to website to filter the games based on search keyword.
+- [x] Hover effect animation for the buttons.
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented features:
+
+<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+
+GIF created with
+[X] ScreenToGif
+
+## Notes
+
+    Features
+
+    1. button hover animation involves a button with an :after pseudo-element that creates a dynamic background effect when hovered. Initially, the pseudo-element is positioned far to the left and slightly upward, with a 45-degree clockwise rotation. On hover, it smoothly shifts horizontally closer to the button's center while maintaining its vertical position and rotation. This movement is animated over 0.5 seconds, using an ease-in-out timing function, resulting in a subtle yet engaging visual transition as the background element slides into view.
+
+    2. The JavaScript code snippet sets up a search functionality for an HTML input field (searchBox). As the user types in the search box, an event listener triggers a real-time filter on a list of games (GAMES_JSON). It compares the input keyword with the names of the games, filtering out those that don't match. The filtered results are then dynamically updated on the webpage by clearing the current content (gamesContainer) and displaying the matching games using the addGamesToPage function. This provides an interactive, instant search experience based on the user's input.
+
+## License
+
+    Copyright [2024] [Srikanth Reddy Mallepally]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ index.js
@@ -0,0 +1,173 @@
+/*****************************************************************************
+ * Challenge 2: Review the provided code. The provided code includes:
+ * -> Statements that import data from games.js
+ * -> A function that deletes all child elements from a parent element in the DOM
+*/
+
+// import the JSON data about the crowd funded games from the games.js file
+import GAMES_DATA from './games.js';
+
+// create a list of objects to store the data about the games using JSON.parse
+const GAMES_JSON = JSON.parse(GAMES_DATA)
+
+// remove all child elements from a parent element in the DOM
+function deleteChildElements(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+/*****************************************************************************
+ * Challenge 3: Add data about each game as a card to the games-container
+ * Skills used: DOM manipulation, for loops, template literals, functions
+*/
+
+const gamesContainer = document.getElementById("games-container");
+
+function addGamesToPage(games) {
+  for (let game of games) {
+    const gameCard = document.createElement("div");
+    gameCard.classList.add("game-card");
+
+    gameCard.innerHTML = `
+      <img src="${game.img}" class="game-img" />
+      <h3>${game.name}</h3>
+      <p>${game.description}</p>
+      <p>Backers: ${game.backers}</p>
+    `;
+
+    gamesContainer.appendChild(gameCard);
+  }
+}
+addGamesToPage(GAMES_JSON);
+
+/*************************************************************************************
+ * Challenge 4: Create the summary statistics at the top of the page displaying the
+ * total number of contributions, amount donated, and number of games on the site.
+ * Skills used: arrow functions, reduce, template literals
+*/
+
+// grab the contributions card element
+const contributionsCard = document.getElementById("num-contributions");
+
+// use reduce() to count the number of total contributions by summing the backers
+const initialValue = 0;
+const totalContributions = GAMES_JSON.reduce(
+  (acc, game) => acc + game.backers,initialValue);
+
+// set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `${totalContributions.toLocaleString("en-US")}`;
+
+// grab the amount raised card, then use reduce() to find the total amount raised
+const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce(
+    (acc,game) => acc + game.pledged, initialValue
+);
+
+// set inner HTML using template literal
+raisedCard.innerHTML = `$${totalRaised.toLocaleString("en-US")}`;
+// grab number of games card and set its inner HTML
+const gamesCard = document.getElementById("num-games");
+const totalGames = GAMES_JSON.length;
+gamesCard.innerHTML = `${totalGames}`;
+
+/*************************************************************************************
+ * Challenge 5: Add functions to filter the funded and unfunded games
+ * total number of contributions, amount donated, and number of games on the site.
+ * Skills used: functions, filter
+*/
+
+// show only games that do not yet have enough funding
+function filterUnfundedOnly() {
+    deleteChildElements(gamesContainer);
+
+    // use filter() to get a list of games that have not yet met their goal
+        const unfunded = GAMES_JSON.filter((un) => un.pledged < un.goal);
+    // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(unfunded);
+
+}
+
+// show only games that are fully funded
+function filterFundedOnly() {
+    deleteChildElements(gamesContainer);
+
+    // use filter() to get a list of games that have met or exceeded their goal
+    const funded = GAMES_JSON.filter((fun) => fun.pledged >= fun.goal);
+    // use the function we previously created to add unfunded games to the DOM
+    addGamesToPage(funded);
+
+}
+
+// show all games
+function showAllGames() {
+    deleteChildElements(gamesContainer);
+
+    // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
+}
+
+// select each button in the "Our Games" section
+const unfundedBtn = document.getElementById("unfunded-btn");
+const fundedBtn = document.getElementById("funded-btn");
+const allBtn = document.getElementById("all-btn");
+
+// add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
+
+/*************************************************************************************
+ * Challenge 6: Add more information at the top of the page about the company.
+ * Skills used: template literals, ternary operator
+*/
+
+// grab the description container
+const descriptionContainer = document.getElementById("description-container");
+
+// use filter or reduce to count the number of unfunded games
+const unfunded = GAMES_JSON.filter((ufun) => ufun.pledged < ufun.goal);
+
+// create a string that explains the number of unfunded games using the ternary operator
+let Str2 = `${unfunded.length >1 ? "games" : "game"}`;
+
+// create a new DOM element containing the template string and append it to the description container
+descriptionContainer.innerHTML+=`<p> A total of ${totalRaised.toLocaleString("en-US")} has been raised for ${totalGames} ${Str2}. Currently, ${unfunded.length} games remains unfunded. We need your help to fund these amazing games! </p>`;
+/************************************************************************************
+ * Challenge 7: Select & display the top 2 games
+ * Skills used: spread operator, destructuring, template literals, sort 
+ */
+
+const firstGameContainer = document.getElementById("first-game");
+const secondGameContainer = document.getElementById("second-game");
+
+const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
+    return item2.pledged - item1.pledged;
+});
+
+// use destructuring and the spread operator to grab the first and second games
+const [ firstGame, secondGame] =sortedGames;
+// create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameEle = document.createElement("p");
+firstGameEle.classList.add("topgame-div");
+firstGameEle.innerHTML =`Top Pleged Game:${firstGame.name}`;
+firstGameContainer.appendChild(firstGameEle);
+// do the same for the runner up item
+const secondGameEle = document.createElement("p");
+secondGameEle.innerHTML =`Top Pleged Game:${secondGame.name}`;
+secondGameEle.classList.add("topgame-div");
+secondGameContainer.appendChild(secondGameEle);
+
+/////Additional features
+
+//search box
+const searchBox = document.getElementById("searchBox");
+
+searchBox.addEventListener("input", (search) => {
+  const searchTerm = search.target.value.toLowerCase();
+  const gameFilter = GAMES_JSON.filter((game) =>
+    game.name.toLowerCase().includes(searchTerm)
+  );
+  deleteChildElements(gamesContainer);
+  addGamesToPage(gameFilter);
+});
